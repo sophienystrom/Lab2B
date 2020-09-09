d09c7cb75536d3847f03e2f7098e87e495358f5a
snystrom <sophiekarennystrom@gmail.com>
2020-09-09 23:53:01 +0000

    Finished code- sole INTEGER version. May update in future to allow decimals

--- main.cs
+++ main.cs
@@ -2,6 +2,28 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.Write("Enter a number: ");
+    var num1 = Console.ReadLine();
+    Console.Write("Enter another number: ");
+    var num2 = Console.ReadLine();
+    int x = Convert.ToInt32 (num1);
+    int y = Convert.ToInt32 (num2);
+    // I originally had this under "double" so that decimals could also be utilized in this code. Will further research c# syntax that can make just the quotient section an integer (personal)
+    var sum = (x + y);
+    Console.WriteLine ("Sum = " + sum);
+    var diff = (x - y);
+    Console.WriteLine ("Difference = " + diff);
+    var product = ( x * y); 
+    Console.WriteLine ("Product = " + product);
+    var quotient = ( x/y );
+    Console.WriteLine ("Quotient = " + quotient);
+    var remainder = ( x % y );
+    Console.WriteLine ("Remainder = " + remainder);
+
+
+
+  
+
+     
   }
 }
